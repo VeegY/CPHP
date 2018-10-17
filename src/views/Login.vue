9d37d4622f1e872ac3c3273bb1066ab264583055
VeegY <kevin.hollmann@tu-dortmund.de>
2018-10-17 13:23:19 +0200

    Added Login view

---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="login-page-container">
+        <div class="input-container">
+            <input type="text" placeholder="Username" v-model="username"/>
+            <input type="password" placeholder="Password" v-model="password"/>
+        </div>
+        <div class="button-container">
+            <button>Login</button>
+            <button>Register</button>
+        </div>
+        <div>
+            <p>Username is: {{username}}</p>
+            <p>Password is: {{password}}</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component({})
+    export default class Login extends Vue {
+        username: string = "";
+        password: string = "";
+    }
+</script>
+
+<style lang="scss">
+
+</style>
